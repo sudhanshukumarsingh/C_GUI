bde7ce5edd08be4c992edccfe7dd54c316dbb0fa
sudhanshu <sudhanshu.github@gmail.com>
2019-05-22 11:02:08 +0530

    Seventh Commit

---
+++ switch_tedious.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(void)
+{
+	int a =	2;
+
+	if (a == 0)
+	{
+		printf("a is equal to 0\n");
+	}
+	else if (a == 1)
+	{
+		printf("a is equal to 1\n");
+	}
+	else
+	{
+		printf("a is greater than 1\n");
+	}
+}
+
+
