c9583a132c846f46b8ceda6461d68f2127aca694
sudhanshu <sudhanshu.github@gmail.com>
2019-05-25 15:40:38 +0530

    Eigth Commit

---
+++ void_pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(void)
+{
+	int intval = 25598283;
+        void *vptr = &intval;
+
+	printf("The value at vptr as an int is %d\n", *((int *) vptr));
+	printf("The value at vptr as an char is %d\n", *((char *) vptr));
+}
+
