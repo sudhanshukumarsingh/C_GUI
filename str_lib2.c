bb12fd3ad0c14895d04851066e8c25ba111bbbd1
sudhanshu <sudhanshu.github@gmail.com>
2019-05-27 19:34:21 +0530

    Eleventh Commit

---
+++ str_lib2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main (void)
+{
+	char str1[10] = "first";
+	char str2[10] = "fire";
+	char *ptr1 = str1, *ptr2 = str2;
+
+	while (*ptr1 != 0 && *ptr2 != 0)
+	{
+	 	if (*ptr1 != *ptr2)
+		{
+			break;
+		}
+		ptr1++;
+		ptr2++;
+	}
+	
+	if (*ptr1 == 0 && *ptr2 == 0)
+	{
+		printf ("The two strings are identical.\n");
+	}
+	else
+	{
+		printf ("The two strings are different.\n");
+	}
+}
+
+    
